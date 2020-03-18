4f5f06e77cf8123530be32a80a45b93b4c4e139d
Roger Oriol <rogi23696@gmail.com>
2020-03-18 20:11:00 +0100

    fix muty dist

--- 2.muty/muty-source-files/muty_dist.erl
+++ 2.muty/muty-source-files/muty_dist.erl
@@ -1,4 +1,4 @@
--module(muty).
+-module(muty_dist).
 -export([start/4, stop/1]).
 
 % We use the name of the module (i.e. lock3) as a parameter to the start procedure. We also provide the average time (in milliseconds) the worker is going to sleep before trying to get the lock (Sleep) and work with the lock taken (Work).
