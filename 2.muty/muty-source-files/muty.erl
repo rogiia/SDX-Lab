bf159213dad5bdd7d92ad0fcbb0619ca2ced1dc0
Roger Oriol <rogi23696@gmail.com>
2020-03-18 20:53:18 +0100

    distributed

--- 2.muty/muty-source-files/muty.erl
+++ 2.muty/muty-source-files/muty.erl
@@ -1,8 +1,6 @@
 -module(muty).
 -export([start/3, stop/0]).
 
-% We use the name of the module (i.e. lock3) as a parameter to the start procedure. We also provide the average time (in milliseconds) the worker is going to sleep before trying to get the lock (Sleep) and work with the lock taken (Work).
-
 start(Lock, Sleep, Work) ->
     register(l1, apply(Lock, start, [1])),
     register(l2, apply(Lock, start, [2])),
