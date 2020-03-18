ac86cf2b438677f8e4e7d8754aab6344d983a550
Roger Oriol <rogi23696@gmail.com>
2020-03-18 20:02:40 +0100

    added muty distributed code

--- 2.muty/muty-source-files/muty.erl
+++ 2.muty/muty-source-files/muty.erl
@@ -1,27 +1,25 @@
 -module(muty).
--export([start/4, stop/1]).
+-export([start/3, stop/0]).
 
 % We use the name of the module (i.e. lock3) as a parameter to the start procedure. We also provide the average time (in milliseconds) the worker is going to sleep before trying to get the lock (Sleep) and work with the lock taken (Work).
 
-start(Lock, Sleep, Work, [Node1,Node2,Node3,Node4]) ->
-    spawn(Node1, fun() -> register(l1,Lock:start(1)), 
-                          register(w1,worker:start("John", l1, Sleep, Work)) end),
-    spawn(Node2, fun() -> register(l2,Lock:start(2)), 
-                          register(w2,worker:start("Ringo", l2, Sleep, Work)) end),
-    spawn(Node3, fun() -> register(l3,Lock:start(3)), 
-                          register(w3,worker:start("Paul", l3, Sleep, Work)) end),
-    spawn(Node4, fun() -> register(l4,Lock:start(4)), 
-                          register(w4,worker:start("George", l4, Sleep, Work)) end),
-    timer:sleep(20),
-    {l1,Node1} ! {peers, [{l2,Node2}, {l3,Node3}, {l4,Node4}]},
-    {l2,Node2} ! {peers, [{l1,Node1}, {l3,Node3}, {l4,Node4}]},
-    {l3,Node3} ! {peers, [{l1,Node1}, {l2,Node2}, {l4,Node4}]},
-    {l4,Node4} ! {peers, [{l1,Node1}, {l2,Node2}, {l3,Node3}]},
-    
+start(Lock, Sleep, Work) ->
+    register(l1, apply(Lock, start, [1])),
+    register(l2, apply(Lock, start, [2])),
+    register(l3, apply(Lock, start, [3])),
+    register(l4, apply(Lock, start, [4])),
+    l1 ! {peers, [l2, l3, l4]},
+    l2 ! {peers, [l1, l3, l4]},
+    l3 ! {peers, [l1, l2, l4]},
+    l4 ! {peers, [l1, l2, l3]},
+    register(w1, worker:start("John", l1, Sleep, Work)),
+    register(w2, worker:start("Ringo", l2, Sleep, Work)),
+    register(w3, worker:start("Paul", l3, Sleep, Work)),
+    register(w4, worker:start("George", l4, Sleep, Work)),
     ok.
 
-stop([Node1,Node2,Node3,Node4]) ->
-    {w1,Node1} ! stop,
-    {w2,Node2} ! stop,
-    {w3,Node3} ! stop,
-    {w4,Node4} ! stop.
+stop() ->
+    w1 ! stop,
+    w2 ! stop,
+    w3 ! stop,
+    w4 ! stop.
