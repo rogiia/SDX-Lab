95a22b95cddb22675141be21c999ca264f0d41b9
Roger Oriol <rogi23696@gmail.com>
2020-05-03 14:01:20 +0200

    first namy implementation

---
+++ 5.namy/namy-source-files/cache.erl
@@ -0,0 +1,20 @@
+-module(cache).
+-export([lookup/2, add/4, remove/2]).
+
+lookup(Name, Cache) ->
+  case lists:keyfind(Name, 1, Cache) of 
+    {Name, Expire, Reply} -> 
+      Now = erlang:monotonic_time(),
+      CurrentTime = erlang:convert_time_unit(Now, native, second),
+      case Expire < CurrentTime of
+        true -> invalid;
+        false -> Reply
+      end;
+    false -> unknown
+  end.
+
+add(Name, Expire, Reply, Cache) ->
+  lists:keystore(Name, 1, Cache, {Name, Expire, Reply}).
+
+remove(Name, Cache) ->
+  lists:keydelete(Name, 1, Cache).
